2f85d9262c102450dc49a8c45fd1fd108f604876
Andrew Hobbs <andrew@hobbservations.com>
2019-06-14 18:28:00 -0700

    updated .gitignore

--- .ipynb_checkpoints/download_data-checkpoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Downloading data"
-curl -SL https://storage.googleapis.com/wandb_datasets/droughtwatch_97K.zip > droughtwatch_97K.zip
-unzip droughtwatch_97K.zip
-rm droughtwatch_97k.zip
-mv droughtwatch_97K/ data/ 
-
